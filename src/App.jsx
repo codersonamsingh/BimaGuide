0061e6b2f8a6895cfd7c9686c7c0cd4f0e3b9d92
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-05-31 00:16:42 +0530

    navbar run

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return(
-    <div>
+    
+      <div>
+      <Navbar />
+      {/* <Routes>
+        <Route path='/' element={<Home sidebar={sidebar}/>} />
+        <Route path='/video/:categoryId/:videoId' element={<Video/>} />
+      </Routes> */}
+
 
     </div>
+
+    
   )
 }
 export default App
